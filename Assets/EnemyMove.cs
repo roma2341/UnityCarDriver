e9b47b5b54506cb7163b2f4da186d2de2e43b85d
romaSHS <roman.z@outsource-to-ukraine.com>
2020-02-21 17:23:37 +0200

    Added enemies

---
+++ Assets/EnemyMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    float speed;
+    void Start()
+    {
+        speed = Random.Range(1f, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 1, 0) * speed * Time.deltaTime);
+    }
+}
